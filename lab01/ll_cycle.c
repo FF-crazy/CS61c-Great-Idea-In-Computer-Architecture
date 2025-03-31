4bbbf273dd348aef1e7c60bd9dcdc8db39fbe6ac
FF_Crazy <13453488571@163.com>
2025-03-31 11:33:27 +0800

    lab02 completefd

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,15 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *fast = head, *slow = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (fast == slow) {
+            return 1;
+        }
+    }
+    
     return 0;
 }
