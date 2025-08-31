c1ae8b6311169c0813f2aa43df67504653be3745
FF_Crazy <13453488571@163.com>
2025-08-31 17:46:47 +0800

    lab07 completed

--- lab07/transpose.c
+++ lab07/transpose.c
@@ -9,8 +9,22 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
     }
 }
 
+static inline int min_int(int a, int b) {
+  return a < b ? a : b;
+}
 /* Implement cache blocking below. You should NOT assume that n is a
  * multiple of the block size. */
-void transpose_blocking(int n, int blocksize, int *dst, int *src) {
-    // YOUR CODE HERE
+void transpose_blocking(int n, int blocksize, int* dst, int* src) {
+  for (int ii = 0; ii < n; ii += blocksize) {
+    int i_end = min_int(ii + blocksize, n);
+    for (int jj = 0; jj < n; jj += blocksize) {
+      int j_end = min_int(jj + blocksize, n);
+      for (int i = ii; i < i_end; ++i) {
+        for (int j = jj; j < j_end; ++j) {
+          dst[j + i * n] = src[i + j * n];
+          // dst[j + i * n] = src[i + j * n];
+        }
+      }
+    }
+  }
 }
